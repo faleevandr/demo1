b52dc92ed65a47fc297bbb8b6b21672c0cc17420
faleevand <faleevandre@gmail.com>
2023-06-14 13:17:56 +0300

    1

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+//import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column
+    private int age;
+}
+
